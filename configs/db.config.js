a49169ba35da82f3ebe365a1f4bed6a58d72f08d
Dwabzy <jspragadeesh@gmail.com>
2021-08-25 01:05:00 +0530

    Update db.config.js

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,13 +1,8 @@
+const DB_HOST = "DB_HOST"
+const DB_USERNAME = "DB_USERNAME"
+const DB_PASSWORD = "DB_PASSWORD"
+const DB_NAME = "DB_NAME"
 
-// const DB_HOST = "DB_HOST"
-// const DB_USERNAME = "DB_USERNAME"
-// const DB_PASSWORD = "DB_PASSWORD"
-// const DB_NAME = "DB_NAME"
-
-const DB_HOST = "localhost"
-const DB_USERNAME = "root"
-const DB_PASSWORD = "Dwabzy"
-const DB_NAME = "assignment"
 
 module.exports = { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME }
 
