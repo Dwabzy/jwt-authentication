7888c8b3f90b45c7a0c31766050e08137e1d7635
Dwabzy <50489305+Dwabzy@users.noreply.github.com>
2021-08-25 19:04:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ cron.schedule('0 0 0 * * *', async () => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
 
 
